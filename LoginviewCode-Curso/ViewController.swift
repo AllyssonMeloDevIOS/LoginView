65e63cbaf86aa22c9972fe4edceb970edfb0edd7
Allysson Melo <allymelox@gmail.com>
2022-12-01 23:47:51 -0300

    Initial Commit

---
+++ LoginviewCode-Curso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginviewCode-Curso
+//
+//  Created by Allysson Melo on 01/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
